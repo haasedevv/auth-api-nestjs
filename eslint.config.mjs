9cb52c531f132b013aa43f42dc10e76279ef0b08
Joseph Haase <joseph.haase.dev@gmail.com>
2025-06-13 18:44:09 -0300

    :wrench: chore: Adds eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,6 +31,12 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: ['./*', '../*'],
+        },
+      ],
     },
   },
 );
