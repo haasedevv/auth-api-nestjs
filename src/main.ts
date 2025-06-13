e0d5508ead1621bb2cf348a82ab65b4827e7d8bc
Joseph Haase <joseph.haase.dev@gmail.com>
2025-06-13 18:45:57 -0300

    :sparkles: feat: Adds cookie parser middleware to app.
    Enables cookie parsing for incoming requests to support
    features that require reading cookies, such as authentication
    and session management.

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { HttpExceptionFilter } from '@/common/exception-filters/http-exception/h
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   const config = new DocumentBuilder()
     .setTitle('Auth api')
